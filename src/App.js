cfe7d550484304b09a5230c475d85d58d641bd1c
Nikhil Patel <nikhilpatel87@yahoo.com>
2021-02-13 22:07:43 -0600

    Add Axios and Row Component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import React from 'react';
 import './App.css';
+import Row from './components/Row';
+import requests from './requests';
 
 function App() {
 	return (
 		<div className="App">
-			<h1>Hello World!</h1>
+			<Row title="NETFLIX ORIGINAL" fetchURL={requests.fetchNetflixOriginals} />
+			<Row title="Trending Now" fetchURL={requests.fetchTrending} />
 		</div>
 	);
 }
---
+++ src/components/Row.js
@@ -0,0 +1,40 @@
+import React, { useEffect, useState } from 'react';
+import axios from '../axios';
+import './Row.css';
+
+const base_url = 'https://image.tmdb.org/t/p/original/';
+function Row({ title, fetchURL }) {
+	const [movies, setMovies] = useState([]);
+
+	//code runs based on a specific condition
+	useEffect(() => {
+		//if [], run once when the row loads, and don't run it again
+		async function fetchData() {
+			const request = await axios.get(fetchURL);
+			setMovies(request.data.results);
+			return request;
+		}
+		fetchData();
+	}, [fetchURL]);
+
+	return (
+		<div className="row">
+			{/* title */}
+			<h2>{title}</h2>
+
+			<div className="row__posters">
+				{movies.map((movie) => (
+					<img
+						key={movie.id}
+						className="row__poster"
+						src={`${base_url}${movie.poster_path}`}
+						alt={movie.name}
+					/>
+				))}
+			</div>
+			{/* container => posters */}
+		</div>
+	);
+}
+
+export default Row;
